6436cedc2bf0ce36ad14003f200ac0548f26490e
despiegk <kristof@incubaid.com>
2018-07-17 16:54:27 +0200

    docserver

--- app/wiki/routes.py
+++ app/wiki/routes.py
@@ -15,54 +15,10 @@ from js9 import j
 def index():
     return redirect("wiki/foundation")
 
-@blueprint.route('/capacity')
-def capacity():
-
-    q=j.clients.grid_capacity.get()
-
-    data = q.api.ListCapacity()[0]
-
-    def fix(row):
-        row2=[]
-        for item in row:
-            item=str(item)
-            item = item.replace(",",";")
-            row2.append(item)
-        return row2
-
-    out= "farmer_id, country, node_id, cru, hru, mru, sru, cat\n"
-
-    for item in data:
-        d = item.as_dict()
-        row=[]
-        row.append(d["farmer_id"])
-        row.append(d["location"]["country"])
-        row.append(d["node_id"])
-        row.append(d["total_resources"]["cru"])
-        row.append(d["total_resources"]["hru"])
-        row.append(d["total_resources"]["mru"])
-        row.append(d["total_resources"]["sru"])
-        os_version = d["os_version"]
-        cat = ""
-        for version in ["1.4.1","1.3.0","development","master"]:
-            if version in os_version:
-                cat = version
-        if cat == "":
-            cat = os_version
-        row.append(cat)
-        row = fix(row)
-        out+= ",".join(row)+"\n"
-
-    return out
-        
-    # j.sal.fs.writeFile("report.csv",out)
-        
-    
 
 @blueprint.route('/<sub>')
 def index_sub(sub):
     return render_template('index_docsify.html')
-
 
 
 @blueprint.route('/<path:subpath>')
